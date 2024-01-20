79ba7ae7a6b4dce81f1b243d65c019287d4818e6
小傅哥 <184172133@qq.com>
2024-01-20 15:05:37 +0800

    feat: 营销服务 第7节：责任链模式处理抽奖规则

--- big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/rule/chain/impl/RuleWeightLogicChain.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/rule/chain/impl/RuleWeightLogicChain.java
@@ -1,12 +1,8 @@
-package cn.bugstack.domain.strategy.service.rule.impl;
+package cn.bugstack.domain.strategy.service.rule.chain.impl;
 
-import cn.bugstack.domain.strategy.model.entity.RuleActionEntity;
-import cn.bugstack.domain.strategy.model.entity.RuleMatterEntity;
-import cn.bugstack.domain.strategy.model.valobj.RuleLogicCheckTypeVO;
 import cn.bugstack.domain.strategy.repository.IStrategyRepository;
-import cn.bugstack.domain.strategy.service.annotation.LogicStrategy;
-import cn.bugstack.domain.strategy.service.rule.ILogicFilter;
-import cn.bugstack.domain.strategy.service.rule.factory.DefaultLogicFactory;
+import cn.bugstack.domain.strategy.service.armory.IStrategyDispatch;
+import cn.bugstack.domain.strategy.service.rule.chain.AbstractLogicChain;
 import cn.bugstack.types.common.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -16,43 +12,36 @@ import java.util.*;
 
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
- * @description 【抽奖前规则】根据抽奖权重返回可抽奖范围KEY
- * @create 2024-01-06 09:57
+ * @description 权重抽奖责任链
+ * @create 2024-01-20 10:38
  */
 @Slf4j
-@Component
-@LogicStrategy(logicMode = DefaultLogicFactory.LogicModel.RULE_WIGHT)
-public class RuleWeightLogicFilter implements ILogicFilter<RuleActionEntity.RaffleBeforeEntity> {
+@Component("rule_weight")
+public class RuleWeightLogicChain extends AbstractLogicChain {
 
     @Resource
     private IStrategyRepository repository;
 
-    public Long userScore = 4500L;
+    @Resource
+    protected IStrategyDispatch strategyDispatch;
+
+    // 根据用户ID查询用户抽奖消耗的积分值，本章节我们先写死为固定的值。后续需要从数据库中查询。
+    public Long userScore = 0L;
 
     /**
-     * 权重规则过滤；
+     * 权重责任链过滤；
      * 1. 权重规则格式；4000:102,103,104,105 5000:102,103,104,105,106,107 6000:102,103,104,105,106,107,108,109
      * 2. 解析数据格式；判断哪个范围符合用户的特定抽奖范围
-     *
-     * @param ruleMatterEntity 规则物料实体对象
-     * @return 规则过滤结果
      */
     @Override
-    public RuleActionEntity<RuleActionEntity.RaffleBeforeEntity> filter(RuleMatterEntity ruleMatterEntity) {
-        log.info("规则过滤-权重范围 userId:{} strategyId:{} ruleModel:{}", ruleMatterEntity.getUserId(), ruleMatterEntity.getStrategyId(), ruleMatterEntity.getRuleModel());
+    public Integer logic(String userId, Long strategyId) {
+        log.info("抽奖责任链-权重开始 userId: {} strategyId: {} ruleModel: {}", userId, strategyId, ruleModel());
 
-        String userId = ruleMatterEntity.getUserId();
-        Long strategyId = ruleMatterEntity.getStrategyId();
-        String ruleValue = repository.queryStrategyRuleValue(ruleMatterEntity.getStrategyId(), ruleMatterEntity.getAwardId(), ruleMatterEntity.getRuleModel());
+        String ruleValue = repository.queryStrategyRuleValue(strategyId, ruleModel());
 
         // 1. 根据用户ID查询用户抽奖消耗的积分值，本章节我们先写死为固定的值。后续需要从数据库中查询。
         Map<Long, String> analyticalValueGroup = getAnalyticalValue(ruleValue);
-        if (null == analyticalValueGroup || analyticalValueGroup.isEmpty()) {
-            return RuleActionEntity.<RuleActionEntity.RaffleBeforeEntity>builder()
-                    .code(RuleLogicCheckTypeVO.ALLOW.getCode())
-                    .info(RuleLogicCheckTypeVO.ALLOW.getInfo())
-                    .build();
-        }
+        if (null == analyticalValueGroup || analyticalValueGroup.isEmpty()) return null;
 
         // 2. 转换Keys值，并默认排序
         List<Long> analyticalSortedKeys = new ArrayList<>(analyticalValueGroup.keySet());
@@ -78,22 +67,21 @@ public class RuleWeightLogicFilter implements ILogicFilter<RuleActionEntity.Raff
                 .findFirst()
                 .orElse(null);
 
+        // 4. 权重抽奖
         if (null != nextValue) {
-            return RuleActionEntity.<RuleActionEntity.RaffleBeforeEntity>builder()
-                    .data(RuleActionEntity.RaffleBeforeEntity.builder()
-                            .strategyId(strategyId)
-                            .ruleWeightValueKey(analyticalValueGroup.get(nextValue))
-                            .build())
-                    .ruleModel(DefaultLogicFactory.LogicModel.RULE_WIGHT.getCode())
-                    .code(RuleLogicCheckTypeVO.TAKE_OVER.getCode())
-                    .info(RuleLogicCheckTypeVO.TAKE_OVER.getInfo())
-                    .build();
+            Integer awardId = strategyDispatch.getRandomAwardId(strategyId, analyticalValueGroup.get(nextValue));
+            log.info("抽奖责任链-权重接管 userId: {} strategyId: {} ruleModel: {} awardId: {}", userId, strategyId, ruleModel(), awardId);
+            return awardId;
         }
 
-        return RuleActionEntity.<RuleActionEntity.RaffleBeforeEntity>builder()
-                .code(RuleLogicCheckTypeVO.ALLOW.getCode())
-                .info(RuleLogicCheckTypeVO.ALLOW.getInfo())
-                .build();
+        // 5. 过滤其他责任链
+        log.info("抽奖责任链-权重放行 userId: {} strategyId: {} ruleModel: {}", userId, strategyId, ruleModel());
+        return next().logic(userId, strategyId);
+    }
+
+    @Override
+    protected String ruleModel() {
+        return "rule_weight";
     }
 
     private Map<Long, String> getAnalyticalValue(String ruleValue) {
