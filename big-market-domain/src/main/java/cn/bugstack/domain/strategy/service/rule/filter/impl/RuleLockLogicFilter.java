1598bd3d4e4ac8c7acd457f34b68f22f69665b41
小傅哥 <184172133@qq.com>
2024-01-22 22:21:56 +0800

    fix: RuleLockLogicFilter 防止 NPE 判断，RuleWeightLogicChain 权重配置校验完善

--- big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/rule/filter/impl/RuleLockLogicFilter.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/rule/filter/impl/RuleLockLogicFilter.java
@@ -8,6 +8,7 @@ import cn.bugstack.domain.strategy.service.annotation.LogicStrategy;
 import cn.bugstack.domain.strategy.service.rule.filter.ILogicFilter;
 import cn.bugstack.domain.strategy.service.rule.filter.factory.DefaultLogicFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -34,10 +35,20 @@ public class RuleLockLogicFilter implements ILogicFilter<RuleActionEntity.Raffle
 
         // 查询规则值配置；当前奖品ID，抽奖中规则对应的校验值。如；1、2、6
         String ruleValue = repository.queryStrategyRuleValue(ruleMatterEntity.getStrategyId(), ruleMatterEntity.getAwardId(), ruleMatterEntity.getRuleModel());
-        long raffleCount = Long.parseLong(ruleValue);
+        if (StringUtils.isBlank(ruleValue)) RuleActionEntity.<RuleActionEntity.RaffleCenterEntity>builder()
+                .code(RuleLogicCheckTypeVO.ALLOW.getCode())
+                .info(RuleLogicCheckTypeVO.ALLOW.getInfo())
+                .build();
+
+        long raffleCount = 0L;
+        try {
+            raffleCount = Long.parseLong(ruleValue);
+        } catch (Exception e) {
+            throw new RuntimeException("规则过滤-次数锁异常 ruleValue: " + ruleValue + " 配置不正确");
+        }
 
         // 用户抽奖次数大于规则限定值，规则放行
-        if (userRaffleCount>= raffleCount) {
+        if (userRaffleCount >= raffleCount) {
             return RuleActionEntity.<RuleActionEntity.RaffleCenterEntity>builder()
                     .code(RuleLogicCheckTypeVO.ALLOW.getCode())
                     .info(RuleLogicCheckTypeVO.ALLOW.getInfo())
